de50eb77e5196a6c1da0790a17dfe719772f8b33
Bati <no-reply@batijs.dev>
2025-01-23 19:13:23 +0530

    scaffold Vike app with Bati

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+import { dbSqlite } from "./database/drizzle/db";
+
+declare module "telefunc" {
+  namespace Telefunc {
+    interface Context {
+      db: ReturnType<typeof dbSqlite>;
+    }
+  }
+}
+
+declare global {
+  namespace Vike {
+    interface PageContext {
+      db: ReturnType<typeof dbSqlite>;
+    }
+  }
+}
+
+// biome-ignore lint/complexity/noUselessEmptyExport: ensure that the file is considered as a module
+export {};
---
+++ pages/+Head.tsx
@@ -0,0 +1,15 @@
+// https://vike.dev/Head
+
+import React from "react";
+import logoUrl from "../assets/logo.svg";
+
+import { ColorSchemeScript } from "@mantine/core";
+
+export default function HeadDefault() {
+  return (
+    <>
+      <link rel="icon" href={logoUrl} />
+      <ColorSchemeScript />
+    </>
+  );
+}
